74b3bb87faa2f80dc67daef209ca34b19f2af2d0
preetkamal <preetkamalsingh986@gmail.com>
2025-05-14 17:19:25 -0700

    Changes in Result

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -2,6 +2,9 @@ import { useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import FlightList from '../components/FlightList';
 import NoResults from '../components/NoResults';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Results() {
     const [searchParams] = useSearchParams();
@@ -9,8 +12,10 @@ export default function Results() {
     const to = searchParams.get('to');
     const [flights, setFlights] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate();
+
     useEffect(() => {
-        fetch('/src/data/flights.json')
+        setTimeout(()=>{fetch('/src/data/flights.json')
         .then((res) => res.json())
         .then((data) => {
             const results = data.filter((flight) =>
@@ -19,15 +24,23 @@ export default function Results() {
         );
         setFlights(results);
         setIsLoading(false);
-       });
+       })},1000);
     }, [from, to]);
-    if (isLoading) return <p>Loading flights...</p>;
-    if (flights.length === 0) return <NoResults from={from} to={to} />;;
-
+    if (isLoading) return <CircularProgress /> ;
+    if (flights.length === 0) 
+      return (
+        <div>
+          <NoResults from={from} to={to} />
+          <Button variant="outlined"   onClick={() => {
+            navigate('/');}}>BackToHome</Button>
+        </div>
+    )
     return (
       <div>
         <h2>Flights from {from} to {to}</h2>
         <FlightList flights={flights} />
+        <Button variant="outlined"   onClick={() => {
+          navigate('/');}}>BackToHome</Button>
       </div>
     );
 }
